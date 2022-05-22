a1b2325155f684a3bd09faafb4635a7725535ffe
Jason Zhang <jasonjzhang17@gmail.com>
2022-05-22 09:37:35 -0400

    Documenting caret.js file

--- js/caret.js
+++ js/caret.js
@@ -1,19 +1,24 @@
+// Alias wrapper for document.getElementById()
 function $(elid) {
     return document.getElementById(elid);
 }
 
+// Styles the cursor to the initial state on window load
 var cursor;
 window.onload = init;
 
+// Styles the cursor to be indented 0px
 function init() {
     cursor = $("cursor");
     cursor.style.left = "0px";
 }
 
+// Globally removes newline characters from the given string
 function nl2br(txt) {
     return txt.replace(/\n/g, '');
 }
 
+// Takes keyboard text input and renders it in the typer span
 function typeIt(from, e) {
     e = e || window.event;
     var w = $("typer");
@@ -23,6 +28,7 @@ function typeIt(from, e) {
     }
 }
 
+// Moves the cursor when left and right arrow keys are pressed
 function moveIt(count, e) {
     e = e || window.event;
     var keycode = e.keyCode || e.which;
@@ -33,6 +39,7 @@ function moveIt(count, e) {
     }
 }
 
+// Displays the given text in the console
 function alert(txt) {
     console.log(txt);
 }
